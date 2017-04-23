149e071c1accc6796feb0684be2fe5323d155dce
Rui Paiva <ruivascoliveirapaiva@hotmail.com>
2017-04-23 18:53:21 +0100

    added deleted functions

--- database/user.php
+++ database/user.php
@@ -131,4 +131,15 @@
         $stmt->execute(array($username));
         return $stmt->fetchAll();
     }
+	
+	function getSearchUsers($page, $name, $asc) {
+    global $conn;
+	$param = "%$name%";
+    $stmt = $conn->prepare('SELECT public.Users.first_name, public.Users.last_name, public.Users.email, public.Authenticated_User.photo_url, public.Authenticated_User.username
+							FROM public.Authenticated_User INNER JOIN public.Users ON (public.Authenticated_User.user_id = public.Users.user_id)
+							WHERE (upper(last_name) LIKE upper(?) OR upper(first_name) LIKE upper(?) OR upper(username) LIKE upper(?)) 
+							ORDER BY first_name ' . $asc . 
+							' LIMIT 10 OFFSET ? * 10;');
+    $stmt->execute(array($param, $param, $param, $page));
+    return $stmt->fetchAll();
 ?>
